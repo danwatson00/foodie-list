58b9ab3111a5827859dc1d1787de6f13fc3be7b5
Dan Watson <danwatson00@yahoo.com>
2018-02-27 20:36:59 -0600

    first attempt at xhr call to json function and made index functional

--- js/foodie-list.js
+++ js/foodie-list.js
@@ -1,11 +1,25 @@
 "use strict";
 
+console.log("foodie-list.js is here!");
+
 var restaurants = {};
 
 var cities = {};
 
-let parseData = (data) => {
-
+var xhrRequest = new XMLHttpRequest(); //establishes xhrRequest as the global variable for the XHR request
+restaurants.loadRestaurants = () => {   // states that load restaurants is a function on xhr??
+    return new Promise((resolve, reject) => {   //first line of a Promise
+        if (xhrRequest.status === 200){      //make sure xhr is successful
+            let data = JSON.parse(xhrRequest.responseText); //response text is a property of the variable xhr and it is parsing the data and placing it in data
+            console.log("data: ", data);
+            resolve(xhrRequest.responseText);
+        } else {
+            reject(new Error("XMLHttpRequest Error", xhrRequest.statusText));
+        }
+        xhrRequest.open('GET', "./restaurants.json");
+        xhrRequest.send();
+    });
+    
 };
 
 
