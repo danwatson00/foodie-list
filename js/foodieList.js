1d863241613c06df992756ab582f52cf01cbb106
Dan Watson <danwatson00@yahoo.com>
2018-03-01 17:39:24 -0600

    got all the basics, just need to make it work

--- js/foodieList.js
+++ js/foodieList.js
@@ -4,41 +4,43 @@ console.log("foodieList.js is here!");
 
 var restaurants = {};
 
-let restaurantInd = [];
+let restaurantItem = [];
+
 
 let parseData = (data) => {
-    data.restJson.forEach( (element) => {
-        restaurantInd.push(element);
-        console.log("in parseData:", restaurantInd);
+    data.restJson.forEach((element) => {
+        restaurantItem.push(element);
+        console.log("in parseData:", restaurantItem);
     });
-    console.log("in parseData:", restaurantInd);
-    /* console.log("element", element); */
+    console.log("in parseData:", restaurantItem);
     console.log("data", data);
-    return restaurantInd;
+    return restaurantItem;
 };
 
 
 restaurants.getRestaurants = () => {
-    console.log("in getRestaurants", restaurantInd);
-    return restaurantInd;
+    console.log("in getRestaurants", restaurantItem);
+    return restaurantItem;
 };
 
 var xhrRequest = new XMLHttpRequest(); //establishes xhrRequest as the global variable for the XHR request
 
-restaurants.loadRestaurants = () => {   // states that load restaurants is a function on xhr??
-    return new Promise((resolve, reject) => {   //first line of a Promise
-        if (xhrRequest.status === 200){      //make sure xhr is successful
+restaurants.loadRestaurants = () => {               // what is loadRestaurants?
+    return new Promise((resolve, reject) => {         //first line of a Promise
+        if (xhrRequest.status === 200){                //make sure xhr is successful
             let data = JSON.parse(xhrRequest.responseText); //response text is a property of the variable xhr and it is parsing the data and placing it in data
-            console.log("data: ", data);
-            resolve(xhrRequest.responseText);
+            console.log("data: ", data);              //console log the parsed data??
+            resolve(xhrRequest.responseText);        //resolve the request = fulfill the promise
         } else {
-            reject(new Error("XMLHttpRequest Error", xhrRequest.statusText));
+            reject(new Error("XMLHttpRequest Error", xhrRequest.statusText));  //reject the request
         }
         xhrRequest.open('GET', "./restaurants.json");
         xhrRequest.send();
     });
     
 };
+
+
 
 console.log("restaurants", restaurants);
 
--- js/main.js
+++ js/main.js
@@ -3,3 +3,32 @@
 console.log("main js here");
 
 let list = require("./foodieList");
+
+let showItems = (restaurantData) => {
+    let restDisplay = document.getElementById("rest-display");
+
+    restaurantData.forEach( (restaurant) => {
+        let restBlock = buildRestItem(restaurant);
+        restDisplay.innerHTML += restBlock;
+    });
+};
+
+
+let buildRestItem = (rest) => {
+    let block = "",
+        wrapper = `<section class="block-wrapper" style="border: 2px solid #93908F; background-color:#000000">`,
+        title = `<h3>${rest.restaurant}</h3>`;
+        return block;
+};
+
+
+let restPromise = list.restaurants.loadRestaurants()
+.then(
+    (resolve) => {
+    console.log("resolve", resolve);
+    showItems(resolve);
+},
+(reject) => {
+    console.log("Oops SOmething went wrong",reject);
+
+});
