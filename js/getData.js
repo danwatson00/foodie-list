21516258cba61af144c509b0235beeed80e2df3c
Dan Watson <danwatson00@yahoo.com>
2018-03-02 11:55:30 -0600

    got it printing to dom

---
+++ js/getData.js
@@ -0,0 +1,80 @@
+"use strict";
+
+console.log("getData.js is here!");
+
+var restaurants = {};
+
+let restaurantItems = [];
+
+var cities = {};
+
+let citiesItems = [];
+
+//GET RESTAURANT DATA
+
+let parseData = (data) => {
+    data.restJson.forEach((element) => {
+        restaurantItems.push(element);
+        // console.log("in parseData:", restaurantItems);
+    });
+    // console.log("in parseData:", restaurantItems);
+    // console.log("data", data);
+    return restaurantItems;
+};
+
+restaurants.getRestaurants = () => {
+    console.log("in getRestaurants", restaurantItems);
+    return restaurantItems;
+};
+
+restaurants.loadRestaurants = () => {               // what is loadRestaurants?
+    return new Promise( (resolve, reject) => {         //first line of a Promise
+        let request = new XMLHttpRequest(); //establishes request as the global variable for the XHR request
+        request.onload = function (){
+            if (request.status === 200){                //make sure xhr is successful
+                let data = JSON.parse(request.responseText); //response text is a property of the variable xhr and it is parsing the data and placing it in data
+                console.log("data: ", data);              //console log the parsed data??
+                resolve(parseData(data));        //resolve the request = fulfill the promise
+            } else {
+                reject(new Error("XMLHttpRequest Error", request.statusText));  //reject the request
+            }
+        };
+        request.open('GET', "../js/restaurants.json");
+        request.send();
+    });
+    
+};
+
+//GET CITIES DATA
+
+cities.getCities = () => {
+    console.log("in getCities", citiesItems);
+    return citiesItems;
+};
+
+let parseCities = (data) => {
+    data.JSONcities.forEach((element) => {
+        citiesItems.push(element);
+    });
+    return citiesItems;
+};
+
+
+cities.loadCities = () => {
+    return new Promise((resolve, reject) => {
+        let xhrRequest = new XMLHttpRequest();
+        xhrRequest.onload = function (){
+            if (xhrRequest.status === 200){
+                let data = JSON.parse(xhrRequest.responseText);
+                console.log("city data:", data);
+                resolve(parseCities(data));
+            } else {
+                reject(new Error("SMLHttpRequest Error", xhrRequest.statusText));
+            }
+        };
+        xhrRequest.open('GET', "../js/cities.json");
+        xhrRequest.send();
+    });
+};
+
+module.exports = { restaurants, cities };
--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@
 
 console.log("main js here");
 
-let list = require("./foodieList");
+let db = require("./getData");  
+
+var allRestData;
 
 let showItems = (restaurantData) => {
     let restDisplay = document.getElementById("rest-display");
-
+    console.log("restaurantData", restaurantData);
     restaurantData.forEach( (restaurant) => {
+        console.log(restaurant.restaurant);
         let restBlock = buildRestItem(restaurant);
-        restDisplay.innerHTML += restBlock;
+        restDisplay.innerHTML += `<section class="block-wrapper" style="border: 2px solid #93908F; background-color:#000000">`;
+        restDisplay.innerHTML += `<h3>${restaurant.restaurant}</h3>`;
     });
 };
 
@@ -17,18 +21,23 @@ let showItems = (restaurantData) => {
 let buildRestItem = (rest) => {
     let block = "",
         wrapper = `<section class="block-wrapper" style="border: 2px solid #93908F; background-color:#000000">`,
-        title = `<h3>${rest.restaurant}</h3>`;
-        return block;
+        title = `<h3>${rest}.restaurant</h3>`;
+        // return block;
 };
 
 
-let restPromise = list.restaurants.loadRestaurants()
+let restPromise = db.restaurants.loadRestaurants()
+    // console.log("restPromise", restPromise);
 .then(
     (resolve) => {
-    console.log("resolve", resolve);
+    console.log("then resolve", resolve);
+    allRestData = resolve;
+    //add a function to sort HERE when ready 
     showItems(resolve);
 },
 (reject) => {
-    console.log("Oops SOmething went wrong",reject);
+    console.log("Oops Something went wrong",reject);
 
 });
+
+// module.exports(allRestData);
