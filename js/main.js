35438d7e6638756d6b15f273ba56b1328f8e5d4b
Dan Watson <danwatson00@yahoo.com>
2018-03-02 21:16:44 -0600

    working on cities

--- js/main.js
+++ js/main.js
@@ -4,37 +4,23 @@ console.log("main js here");
 
 let db = require("./getData"); 
 
-// console.log("restaurants in main", restaurants);
 
-// var allRestData;
-
+//show items function prints data to the DOM
 let showItems = (restaurantData) => {
     let restDisplay = document.getElementById("rest-display");
     console.log("restaurantData", restaurantData);
     restaurantData.forEach( (restaurant) => {
-        // console.log(restaurant.restaurant);
-        let restBlock = buildRestItem(restaurant);
-        restDisplay.innerHTML += `<section class="block-wrapper" style="border: 2px solid #93908F; background-color:#000000">`;
-        restDisplay.innerHTML += `<h3>${restaurant.restaurant}</h3>`;
-        restDisplay.innerHTML += `<h4><strong>My Rating: ${restaurant.my_rating}</strong></h4><p>Last Visited: ${restaurant.date_visited}</p>`;
+        // let restBlock = buildRestItem(restaurant);
+        restDisplay.innerHTML += `<section class="block-wrapper"><h3>${restaurant.restaurant}</h3><h4><strong>My Rating: ${restaurant.my_rating}</strong></h4><p>Last Visited: ${restaurant.date_visited}</p></section>`;
+        
     });
 };
 
 
-let buildRestItem = (rest) => {
-    let block = "",
-        wrapper = `<section class="block-wrapper" style="border: 2px solid #93908F; background-color:#000000">`,
-        title = `<h3>${rest}.restaurant</h3>`;
-        // return block;
-};
-
-
 let restPromise = db.restaurants.loadRestaurants()
-    // console.log("restPromise", restPromise);
 .then(
     (resolve) => {
     console.log("then resolve", resolve);
-    // allRestData = resolve;
         let sortedData = resolve.sort(function (a, b) {
             return b.my_rating - a.my_rating;
         });
@@ -42,7 +28,20 @@ let restPromise = db.restaurants.loadRestaurants()
 },
 (reject) => {
     console.log("Oops Something went wrong",reject);
-
 });
 
+//Cities Promise
+// let citiesPromise = db.cities.loadCities()
+// .then(
+//     (resolve) => 
+// )
+
+ 
+// This builds each restaurant item or card
+// let buildRestItem = (rest) => {
+//     let block = "",
+//         wrapper = `<section class="block-wrapper" style="border: 2px solid #93908F; background-color:#000000">`,
+//         title = `<h3>${rest}.restaurant</h3>`;
+//         // return block;
+// };
 // module.exports = {allRestData};
